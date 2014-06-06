45aa57445033b5cde33be05bf40a00c4f553df35
Nabilla Sabbaha <nabillasabbaha@gmail.com>
2014-06-06 18:47:57 +0700

    Versi 2

---
+++ PepakDigital/Controllers/ParamasastraUserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PepakDigital;
+
+namespace PepakDigital.Controllers
+{
+    public class ParamasastraUserController : Controller
+    {
+        private DigitalPepakEntities db = new DigitalPepakEntities();
+
+        // GET: /ParamasastraUser/
+        public ActionResult Index()
+        {
+            var paramasastra = db.Paramasastra.Include(p => p.Kategori);
+            return View(paramasastra.ToList());
+        }
+
+       
+    }
+}
